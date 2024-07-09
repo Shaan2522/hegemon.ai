939b01b263c80bf0e47c1eeab706f7878d202f7a
Shantanu Wani <shantanu2522@gmail.com>
2024-07-09 23:16:18 +0530

    launch updates

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
 
 @app.route('/get_response', methods=['POST'])
 def get_response():
-    user_input = request.json['user_input']
+    user_input = request.json['message']
     response = hegemon_chatbot.generate_response(user_input)
     return jsonify({'response': response})
 
