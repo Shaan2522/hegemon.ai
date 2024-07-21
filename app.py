716405efa716a855f9741834c1eea996d3cb5d9c
Shantanu Wani <shantanu2522@gmail.com>
2024-07-21 17:14:49 +0530

    sunday 21st update

--- app.py
+++ app.py
@@ -4,14 +4,40 @@ import hegemon_chatbot as hegemon_chatbot
 app = Flask(__name__)
 
 @app.route('/')
+def landing():
+    return render_template('landingpage.html')
+
+@app.route('/index')
 def index():
     return render_template('index.html')
 
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    # Implement your authentication logic here
+    # For now, we'll just redirect to index.html
+    return redirect(url_for('index'))
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    username = request.form['username']
+    password = request.form['password']
+    # Implement your signup logic here
+    # For now, we'll just redirect to index.html
+    return redirect(url_for('index'))
+
 @app.route('/get_response', methods=['POST'])
 def get_response():
-    user_input = request.json['message']
-    response = hegemon_chatbot.generate_response(user_input)
-    return jsonify({'response': response})
+    try:
+        user_input = request.json['message']
+        response = hegemon_chatbot.generate_response(user_input)
+        if response:
+            return jsonify({'response': response})
+        else:
+            return jsonify({'response': 'Sorry, I didn\'t understand that.'}), 400
+    except Exception as e:
+        return jsonify({'response': 'An error occurred: {}'.format(str(e))}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
